7225fdc1707af7f7c52556dc48fe7e697d3eb146
sam5Lin <1150050235@qq.com>
2019-11-04 23:28:15 +0800

    功能完成

--- app/src/main/java/com/example/phone/MessageActivity.java
+++ app/src/main/java/com/example/phone/MessageActivity.java
@@ -3,22 +3,36 @@ package com.example.phone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView phone;
+    private EditText phone;
+    private EditText content;
+    private Button send;
+    private String number;
+    private String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
         phone = findViewById(R.id.m_number);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        phone.setText(bundle.getString("number"));
-        phone.setOnClickListener(this);
+        number = bundle.getString("number");
+        phone.setText(number);
 
+        content = findViewById(R.id.m_content);
+        send = findViewById(R.id.send);
+        findViewById(R.id.back1).setOnClickListener(this);
+        content.setOnClickListener(this);
+        send.setOnClickListener(this);
     }
 
     @Override
@@ -27,12 +41,20 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.back1:
                 //数据是使用Intent返回
                 Intent intent = new Intent();
-                //把返回数据存入Intent
-                intent.putExtra("result", "My name is linjiqin");
                 //设置返回数据
                 MessageActivity.this.setResult(RESULT_OK, intent);
                 //关闭Activity
                 MessageActivity.this.finish();
+                break;
+            case R.id.send:
+                number = phone.getText().toString();
+                message = content.getText().toString();
+                Intent intent1 = new Intent();                        //创建 Intent 实例
+                intent1.setAction(Intent.ACTION_SENDTO);             //设置动作为发送短信
+                intent1.setData(Uri.parse("smsto:"+number));           //设置发送的号码
+                intent1.putExtra("sms_body", message);              //设置发送的内容
+                startActivity(intent1);                               //启动 Activity
+                break;
         }
 
     }
